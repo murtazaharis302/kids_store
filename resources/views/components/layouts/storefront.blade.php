af53ca888aabbb9686ae341980947bbfbea71a0a
Haris Murtaza <murtazaharis302@gmail.com>
2026-09-12 09:48:44 +0500

    Add static public/style.css asset and fix .cpanel.yml deployment subshell bug

--- resources/views/components/layouts/storefront.blade.php
+++ resources/views/components/layouts/storefront.blade.php
@@ -17,18 +17,10 @@
     @if (file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-        @php
-            $manifestPath = public_path('build/manifest.json');
-            $cssFile = 'assets/app-BzJL9BKc.css';
-            $jsFile = 'assets/app-l0sNRNKZ.js';
-            if (file_exists($manifestPath)) {
-                $manifest = json_decode(@file_get_contents($manifestPath), true);
-                $cssFile = $manifest['resources/css/app.css']['file'] ?? $cssFile;
-                $jsFile = $manifest['resources/js/app.js']['file'] ?? $jsFile;
-            }
-        @endphp
-        <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
-        <script type="module" src="{{ asset('build/' . $jsFile) }}"></script>
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
+        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="/build/assets/app-BzJL9BKc.css">
+        <script type="module" src="/build/assets/app-l0sNRNKZ.js"></script>
     @endif
     @livewireStyles
 
